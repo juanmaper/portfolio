d457af955187b75426abd07849e8bdc5fbb722ce
Juanma <juanmaper30@gmail.com>
2022-09-12 17:20:34 +0200

    Add video as background, adjust position and start development

--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -1,5 +1,17 @@
 <template>
-  <h3>asdasd</h3>
+  <div class="container">
+    <video
+          src="~assets/backgroundVideo.mp4" muted
+          autoplay
+          loop />
+  </div>
+
+  <!-- Botones aqui -->
+
+  <h1 class="text-h3 centerText absolute-center matriverRegularFont">I am Juanma</h1>
+
+
+  <h2>dasndonadnasn</h2>
 </template>
 
 <script>
@@ -8,6 +20,25 @@ export default {
 }
 </script>
 
-<style>
+<style scoped lang="scss">
+
+
+.centerText {
+  // background-color: red;
+  font-size: 70px;
+  color: $vue-primary;
+  text-align: center;
+}
+
+.container {
+  height: 100vh;
+  width: 100%;
+}
+
+video {
+  height: 100%;
+  width: 100%;
+  object-fit: fill;
+}
 
 </style>
